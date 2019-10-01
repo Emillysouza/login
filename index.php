d0ca5a06bbc2f56393e4c0862af625387457b9d8
Emillysouza <emilly_souza@estudante.sc.senai.br>
2019-10-01 19:54:49 -0300

    CDN jQuery validate

--- index.php
+++ index.php
@@ -43,12 +43,12 @@
                 <form action="#" method="post" class="p-2" id="formLogin">
 
                     <div class="form-group">
-                        <input type="text" name="nameUsuario" id="nomeUsuario" placeholder="Nome de Usuário" class="form-control">
+                        <input type="text" name="nameUsuario" id="nomeUsuario" placeholder="Nome de Usuário" class="form-control" required minlenght="5">
 
                     </div>
 
                     <div class="form-group">
-                        <input type="password" name="senhaUsuario" id="senhaUsuario" placeholder="senha" class="form-control">
+                        <input type="password" name="senhaUsuario" id="senhaUsuario" placeholder="senha" class="form-control" required minlenght="6">
 
                     </div>
 
@@ -100,7 +100,7 @@
                     </div>
 
                     <div class="form-group">
-                        <input type="email" name="emailGerarsenha" id="emailGerarsenha" class="form-control" placeholder="E-mail de recuperação de senha">
+                        <input type="email" name="emailGerarsenha" id="emailGerarsenha" class="form-control" placeholder="E-mail de recuperação de senha" required>
                     </div>
 
                     <div class="form-group">
@@ -128,24 +128,25 @@
                 <form action="#" method="post" class="p-2" id="formRegistro">
 
                     <div class="form-group">
-                        <input type="text" name="nomeCompleto" id="nomeCompleto" class="form-control" placeholder="Nome completo">
+                        <input type="text" name="nomeCompleto" id="nomeCompleto" class="form-control" placeholder="Nome completo" required minlength="6">
                     </div>
 
                     <div class="form-group">
-                        <input type="text" name="nomeUsuario" id="nomeUsuario" class="form-control" placeholder="Nome de usuário">
+                        <input type="text" name="nomeUsuario" id="nomeUsuario" class="form-control" placeholder="Nome de usuário" required minlength="5">
                     </div>
 
                     <div class="form-group">
-                        <input type="email" name="emailusuario" id="emailUsuario" class="form-control" placeholder="E-mail">
+                        <input type="email" name="emailusuario" id="emailUsuario" class="form-control" placeholder="E-mail" required>
                     </div>
 
                     <div class="form-group">
-                        <input type="password" name="senhaUsuario" id="senhaUsuario" class="form-control" placeholder="Senha">
+                        <input type="password" name="senhaDoUsuario" id="senhaDoUsuario" class="form-control" placeholder="Senha" required minlength="6">
                     </div>
 
                     <div class="form-group">
-                        <input type="password" name="senhaUsuarioConfirmar" id="senhaUsuarioConfirmar" class="form-control" placeholder="Confirma senha">
+                        <input type="password" name="senhaUsuarioConfirmar" id="senhaUsuarioConfirmar" class="form-control" placeholder="Confirma senha" required minlength="6">
                     </div>
+
 
                     <div class="form-group">
                         <div class="custom-control custom-checkbox">
@@ -155,6 +156,7 @@
                             </label>
                         </div>
                     </div>
+
                     <div form-group>
                         <input type="submit" value=";;Registrar;;" name="btnRegistra" id="btnRegistra" class="btn btn-primary btn-block">
                     </div>
@@ -176,9 +178,10 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js"></script>
 
     <script>
         //Codigo jQuery para mostra e ocultar os formulários

acffba302fad470b91a763a4b3c49bd2051d8cf0
Emillysouza <emilly_souza@estudante.sc.senai.br>
2019-10-01 21:40:02 -0300

    Validação de formulários
    Close#8, Close#7, Close6

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Sistema de Login</title>
+    <link rel="stylesheet" href="https://jqueryvalidation.org/files/demo/site-demos.css">
+
     <style>
         #alerta,
         #caixaSenha,
@@ -52,7 +54,7 @@
 
                     </div>
 
-                    <div class="form-group">
+                    <div class="form-group mt-5">
                         <div class="custom-control custom-checkbox">
 
                             <input type="checkbox" name="lembrar" id="lembrar" class="custom-control-input">
@@ -132,7 +134,7 @@
                     </div>
 
                     <div class="form-group">
-                        <input type="text" name="nomeUsuario" id="nomeUsuario" class="form-control" placeholder="Nome de usuário" required minlength="5">
+                        <input type="text" name="nomeDoUsuario" id="nomeDoUsuario" class="form-control" placeholder="Nome de usuário" required minlength="5">
                     </div>
 
                     <div class="form-group">
@@ -148,7 +150,7 @@
                     </div>
 
 
-                    <div class="form-group">
+                    <div class="form-group mt-4">
                         <div class="custom-control custom-checkbox">
                             <input type="checkbox" name="concordar" id="concordar" class="custom-control-input">
                             <label for="concordar" class="custom-control-label">
@@ -186,6 +188,25 @@
     <script>
         //Codigo jQuery para mostra e ocultar os formulários
         $(function() {
+
+            //Validação de formulários
+            jQuery.validator.setDefaults({
+                success: "valid"
+            });
+            $("#formRegistro").validate({
+                rules: {
+                    senhaDoUsuario: "required",
+                    senhaUsuarioConfirmar: {
+                        equalTo: "#senhaDoUsuario"
+                    }
+                }
+            });
+            
+            $("#formLogin").valedate();
+
+            $("#formSenha").valedate();
+            //Mostra e ocultar fomulário
+
             $("#btnEsqueci").click(function() {
                 $("#caixaLogin").hide(); //Ocultar
                 $("#caixaSenha").show(); //Mostra
@@ -206,6 +227,29 @@
                 $("#caixaRegistro").show(); //Mostrar
             });
 
+            /*
+             * Translated default messages for the jQuery validation plugin.
+             * Locale: PT_BR
+             */
+            jQuery.extend(jQuery.validator.messages, {
+                required: "Este campo &eacute; requerido.",
+                remote: "Por favor, corrija este campo.",
+                email: "Por favor, forne&ccedil;a um endere&ccedil;o eletr&ocirc;nico v&aacute;lido.",
+                url: "Por favor, forne&ccedil;a uma URL v&aacute;lida.",
+                date: "Por favor, forne&ccedil;a uma data v&aacute;lida.",
+                dateISO: "Por favor, forne&ccedil;a uma data v&aacute;lida (ISO).",
+                number: "Por favor, forne&ccedil;a um n&uacute;mero v&aacute;lido.",
+                digits: "Por favor, forne&ccedil;a somente d&iacute;gitos.",
+                creditcard: "Por favor, forne&ccedil;a um cart&atilde;o de cr&eacute;dito v&aacute;lido.",
+                equalTo: "Por favor, forne&ccedil;a o mesmo valor novamente.",
+                accept: "Por favor, forne&ccedil;a um valor com uma extens&atilde;o v&aacute;lida.",
+                maxlength: jQuery.validator.format("Por favor, forne&ccedil;a n&atilde;o mais que {0} caracteres."),
+                minlength: jQuery.validator.format("Por favor, forne&ccedil;a ao menos {0} caracteres."),
+                rangelength: jQuery.validator.format("Por favor, forne&ccedil;a um valor entre {0} e {1} caracteres de comprimento."),
+                range: jQuery.validator.format("Por favor, forne&ccedil;a um valor entre {0} e {1}."),
+                max: jQuery.validator.format("Por favor, forne&ccedil;a um valor menor ou igual a {0}."),
+                min: jQuery.validator.format("Por favor, forne&ccedil;a um valor maior ou igual a {0}.")
+            });
         });
     </script>
 </body>
